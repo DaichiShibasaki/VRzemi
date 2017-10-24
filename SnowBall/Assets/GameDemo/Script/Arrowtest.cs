54822cf766a42152d0110ff79f1a7f11ae25f4ae
kiyotakak <kytk>
2017-10-24 19:20:11 +0900

    デモ

---
+++ SnowBall/Assets/GameDemo/Script/Arrowtest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrowtest : MonoBehaviour {
+
+    [SerializeField] SteamVR_TrackedObject bow; //Controller (left)
+    [SerializeField] SteamVR_TrackedObject arrow; //Controller (right)
+    
+	public GameObject prefab;
+	public Rigidbody attachPoint;
+    public Transform Backward;
+	FixedJoint joint;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var device = SteamVR_Controller.Input((int)arrow.index);
+        Vector3 distance = bow.transform.position - arrow.transform.position;
+        float length = distance.magnitude;
+		if (joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) && length < 0.1f)
+		{
+			var go = GameObject.Instantiate(prefab);
+			go.transform.position = attachPoint.transform.position;
+
+			joint = go.AddComponent<FixedJoint>();
+			joint.connectedBody = attachPoint;
+		}
+		else if (joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			var go = joint.gameObject;
+			var rigidbody = go.GetComponent<Rigidbody>();
+			Object.DestroyImmediate(joint);
+			joint = null;
+			Object.Destroy(go, 15.0f);
+            
+			rigidbody.velocity = distance.normalized * ( ( distance.magnitude / 10 ) * 100 );
+
+			//rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+        }
+
+        if ( joint ) {
+            Vector3 scale = joint.transform.localScale;
+            scale.x = length;
+            scale.z = length;
+            joint.transform.localScale = scale;
+
+            Vector3 pos = joint.transform.position;
+            pos.x = bow.transform.position.x - ( distance.x / 2 );
+            pos.z = bow.transform.position.z - ( distance.z / 2 );
+            joint.transform.position = pos;
+        }
+	}
+
+}
---
+++ SnowBall/Assets/GameDemo/Script/Shottest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shottest : MonoBehaviour {
+    
+	public GameObject prefab;
+	public Rigidbody attachPoint;
+    public Transform Backward;
+	SteamVR_TrackedObject trackedObj;
+	FixedJoint joint;
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var device = SteamVR_Controller.Input((int)trackedObj.index);
+		if (joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			var go = GameObject.Instantiate(prefab);
+			go.transform.position = attachPoint.transform.position;
+
+			joint = go.AddComponent<FixedJoint>();
+			joint.connectedBody = attachPoint;
+		}
+		else if (joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			var go = joint.gameObject;
+			var rigidbody = go.GetComponent<Rigidbody>();
+			Object.DestroyImmediate(joint);
+			joint = null;
+			Object.Destroy(go, 15.0f);
+
+            Vector3 forward = attachPoint.transform.position - Backward.position;
+
+			rigidbody.velocity = forward * 100;
+
+			rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+        }
+	}
+}
