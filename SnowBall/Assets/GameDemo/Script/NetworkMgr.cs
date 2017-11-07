581add3acea59b15f37649e8c00a44cf57097530
akiyama <aki>
2017-11-07 18:56:09 +0900

    ローカルネットワーク対応

---
+++ SnowBall/Assets/GameDemo/Script/NetworkMgr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkMgr : MonoBehaviour {
+    public GameObject objectPrefab;
+    string ip = "172.18.10.127";
+    string port = "1192";
+    bool connected = false;
+
+    public void OnConnectedToServer()
+    {
+        connected = true;
+    }
+
+    public void OnServerInitialized()
+    {
+        connected = true;
+
+    }
+
+
+    public void OnGUI()
+    {
+        if (!connected)
+        {
+            if (GUI.Button(new Rect(10, 10, 90, 90), "Client"))
+            {
+                Network.Connect(ip, int.Parse(port));
+            }
+            if (GUI.Button(new Rect(10, 110, 90, 90), "Master"))
+            {
+                Network.InitializeServer(10, int.Parse(port), false);
+            }
+        }
+    }
+}
---
+++ SnowBall/Assets/GameDemo/Script/PlayerPop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPop : MonoBehaviour {
+
+
+    GameObject Player;
+    public GameObject PlayerPrefab;
+    public Transform startpoint1;
+    public Transform startpoint2;
+
+
+    // Use this for initialization
+    void Start () {
+	}
+
+    private void OnServerInitialized()
+    {
+        Player = Network.Instantiate(PlayerPrefab, startpoint1.position, startpoint1.rotation, 0) as GameObject;
+        Debug.Log("Server");
+    }
+    private void OnConnectedToServer()
+    {
+        Player = Network.Instantiate(PlayerPrefab, startpoint2.position, startpoint2.rotation, 0) as GameObject;
+        Debug.Log("Client");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
