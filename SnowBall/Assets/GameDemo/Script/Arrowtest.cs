cfba13e0561557e73df81638ad2d47bc039e0463
kiyotaka <kytk>
2017-12-19 19:11:06 +0900

    vive振動・LiveCamera

--- SnowBall/Assets/GameDemo/Script/Arrowtest.cs
+++ SnowBall/Assets/GameDemo/Script/Arrowtest.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Arrowtest : MonoBehaviour {
 
-    [SerializeField] Transform left; //Controller (left)
-    [SerializeField] Transform right; //Controller (right)
-    [SerializeField] SteamVR_TrackedObject arrow; //Controller (right)
+    [SerializeField] Transform left = null; //Controller (left)
+    [SerializeField] Transform right = null; //Controller (right)
+    [SerializeField] SteamVR_TrackedObject arrow_controller = null; //Controller (right)
+    [SerializeField] SteamVR_TrackedObject bow_controller = null; //Controller (left)
     [SerializeField] float _speed = 100;
 	public GameObject prefab;
 	public Rigidbody attachPoint;
 	FixedJoint joint;
     
+	const ushort DRAW_VIVE_POW = 1000;
+	const ushort SHOT_VIVE_POW = 3999;
+	float _vive_length = 0;
+	public float _before_length = 0;
+	const float VIVE_LENGTH = 0.1f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -19,7 +26,7 @@ public class Arrowtest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var device = SteamVR_Controller.Input((int)arrow.index);
+		var device = SteamVR_Controller.Input((int)arrow_controller.index);
         Vector3 distance = right.position - left.position;
         float length = distance.magnitude;
 		if (joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) && length < 0.2f)
@@ -29,6 +36,8 @@ public class Arrowtest : MonoBehaviour {
 
 			joint = go.AddComponent<FixedJoint>();
 			joint.connectedBody = attachPoint;
+			_vive_length = length - VIVE_LENGTH;
+			_before_length = length;
 		}
 
 		if (joint)
@@ -42,6 +51,20 @@ public class Arrowtest : MonoBehaviour {
 			joint.transform.localScale = scale;
 			joint.transform.position = ( left.position + right.position ) / 2;
 			
+			//バイブレーション
+			if ( length > _vive_length ) {
+				device.TriggerHapticPulse(DRAW_VIVE_POW);
+				SteamVR_Controller.Input((int)bow_controller.index).TriggerHapticPulse(DRAW_VIVE_POW);
+				_vive_length += VIVE_LENGTH;
+				_before_length = length;
+			}
+
+			if ( length - _before_length < -0.1 ) {
+				_vive_length = length;
+				_before_length = length;
+			}
+
+
 			if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
 			{
 				var go = joint.gameObject;
@@ -51,7 +74,14 @@ public class Arrowtest : MonoBehaviour {
 				joint = null;
 				Object.Destroy(go, 15.0f);
 				rigidbody.velocity = distance.normalized * _speed;
+				
+				//バイブレーション
+				device.TriggerHapticPulse(SHOT_VIVE_POW);
+				SteamVR_Controller.Input((int)bow_controller.index).TriggerHapticPulse(SHOT_VIVE_POW);
 			}
+		} else {
+			_vive_length = 0;
+			_before_length = 0;
 		}
 	}    
 }
---
+++ SnowBall/Assets/GameDemo/Script/LiveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveCamera : MonoBehaviour {
+	
+	[SerializeField] GameObject _main;
+	[SerializeField] GameObject [] _live;
+	int _current_camera_idx = 0;
+	const int MAX_LIVE_CAMERA = 4;
+
+	private void Start( ) {
+		UnityEngine.VR.VRSettings.showDeviceView = false;
+	}
+
+	private void Update( ) {
+		if ( Input.GetKeyDown( KeyCode.Space ) ) {
+			_live[_current_camera_idx].SetActive( false );
+			_live[(_current_camera_idx + 1) % MAX_LIVE_CAMERA].SetActive( true );
+			_current_camera_idx = ( _current_camera_idx + 1 ) % MAX_LIVE_CAMERA;
+		}
+	}
+}
